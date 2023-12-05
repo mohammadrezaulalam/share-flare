e95beeaba8f789103ca1572176a53f5626e15b80
Mohammad Rezaul Alam <mail.rezaulalam@gmail.com>
2023-12-06 02:21:56 +0600

    modifed folder

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'package:share_flare/presentation/ui/screen/home_page.dart';
+import 'package:share_flare/presentation/ui/screens/home_page.dart';
 import 'package:share_flare/application/state_holder_binder.dart';
-import 'package:share_flare/presentation/ui/screens/search_screen.dart';
-
 
 //import '../presentation/ui/utilities/colors.dart';
 import '../presentation/ui/utilities/theme/theme.dart';
@@ -15,16 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-   // Get.changeTheme(Get.isDarkMode? ThemeData.light(): ThemeData.dark());
+    // Get.changeTheme(Get.isDarkMode? ThemeData.light(): ThemeData.dark());
     return GetMaterialApp(
-       initialBinding: StateHolderBinder(),
+      initialBinding: StateHolderBinder(),
       debugShowCheckedModeBanner: false,
       title: 'Share Flare',
       theme: SFAppTheme.lightTheme,
       darkTheme: SFAppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
-
 }
