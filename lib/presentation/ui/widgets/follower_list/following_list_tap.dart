796fdb20538a3c68a973f65b33da4b7eff8921b3
Kazi Habiba <kazihabiba125@gmail.com>
2023-12-06 01:01:45 +0600

    Follow screen has been created!

--- lib/presentation/ui/widgets/follower_list/following_list_tap.dart
+++ lib/presentation/ui/widgets/follower_list/following_list_tap.dart
@@ -21,7 +21,7 @@ class _FollowingListTapState extends State<FollowingListTap> {
       children: [
 
         Padding(
-          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -54,7 +54,7 @@ class _FollowingListTapState extends State<FollowingListTap> {
                 ],
               ),
               const SizedBox(
-                width: 110,
+                width: 120,
               ),
               Expanded(
                 child:
