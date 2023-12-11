f909d85dd3d28bafbc2e5fc0fe1fd71cc9c10069
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-12 02:10:05 +0600

    #SFSMA-58 snackbar message show for both registion successful or failed

--- lib/presentation/state_holders/sign_up_controller.dart
+++ lib/presentation/state_holders/sign_up_controller.dart
@@ -10,7 +10,8 @@ import 'package:share_flare/presentation/ui/utilities/auth_constant.dart';
 import 'package:flutter/material.dart';
 
 import '../ui/screens/main_bottom_nav_screen.dart';
-class SignUpController extends GetxController{
+
+class SignUpController extends GetxController {
   static SignUpController instance = Get.find();
 
   //text controller
@@ -30,103 +31,117 @@ class SignUpController extends GetxController{
   bool get isLoading => _isLoading;
 
   //setter method
-   void isLoadingCallInAppBar(bool value){
-    _isLoading=value;
+  void isLoadingCallInAppBar(bool value) {
+    _isLoading = value;
     clearTheControllers();
     update();
   }
+
   //clear the controller
-  void clearTheControllers(){
+  void clearTheControllers() {
     emailTE.clear();
     passwordTE.clear();
     rePasswordTE.clear();
     firstNameTE.clear();
     lastNameTE.clear();
     userNameTE.clear();
-    _imagePath.value='';
+    _imagePath.value = '';
   }
 
   //picked Image
-String get profilePhoto => _imagePath.value;
+  String get profilePhoto => _imagePath.value;
 
-
-  void pickedImage()async{
-  final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
-  if(pickedImage != null){
-    //put image path
-    _imagePath.value=pickedImage.path.toString();
-    Get.snackbar('Profile Picture',
-        'You have successfully selected your profile picture!');
+  void pickedImage() async {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      //put image path
+      _imagePath.value = pickedImage.path.toString();
+      Get.snackbar('Profile Picture',
+          'You have successfully selected your profile picture!');
+    }
   }
 
-
-}
-
 //method for upload the firebase storage(image upload)
-Future<String> _uploadToStorage(String imagepath) async{
-  File image = File(imagepath); // string to File convert
-  Reference ref = firebaseStorage
-      .ref()
-      .child('profilePics')
-      .child(firebaseAuth.currentUser!.uid);
-  UploadTask uploadTask = ref.putFile(image);
-  TaskSnapshot snap = await uploadTask;
-  String downloadUrl = await snap.ref.getDownloadURL();
-  return downloadUrl;
-}
+  Future<String> _uploadToStorage(String imagepath) async {
+    File image = File(imagepath); // string to File convert
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
 
 //registering the user, make this method for Future<bool> only for use await and .then() method in signup screen
-  Future<bool> registerUser()async{
+  Future<bool> registerUser() async {
+    //make sure every parameter has data
+    try {
+      if (firstNameTE.text.isNotEmpty &&
+          lastNameTE.text.isNotEmpty &&
+          userNameTE.text.isNotEmpty &&
+          emailTE.text.isNotEmpty &&
+          // image !=null &&
+          profilePhoto.isNotEmpty &&
+          passwordTE.text.isNotEmpty) {
+        _isLoading = true;
+        update();
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+            email: emailTE.text.trim(), password: passwordTE.text);
+        String downloadUrl = await _uploadToStorage(profilePhoto);
 
-  //make sure every parameter has data
-  try{
-    if(
-    firstNameTE.text.isNotEmpty &&
-     lastNameTE.text.isNotEmpty &&
-     userNameTE.text.isNotEmpty &&
-     emailTE.text.isNotEmpty &&
-     // image !=null &&
-     profilePhoto.isNotEmpty &&
-     passwordTE.text.isNotEmpty
-    ){
-      _isLoading = true;
-      update();
-     UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(email:  emailTE.text.trim(), password:  passwordTE.text);
-     String downloadUrl = await _uploadToStorage(profilePhoto);
+        //add model class
+        UserModelRegistration user = UserModelRegistration(
+            firstName: firstNameTE.text,
+            lastName: lastNameTE.text,
+            userName: userNameTE.text,
+            profilePhoto: downloadUrl,
+            email: emailTE.text,
+            uid: cred.user!.uid);
 
-     //add model class
-      UserModelRegistration user = UserModelRegistration(firstName: firstNameTE.text, lastName: lastNameTE.text, userName: userNameTE.text, profilePhoto: downloadUrl, email: emailTE.text, uid: cred.user!.uid);
-
-      //add data to firestore database
-      await fireStore
-     .collection('users')
-     .doc(cred.user!.uid)
-     .set(user.toJson())
-     .then((value){
+        //add data to firestore database
+        await fireStore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson())
+            .then((value) {
+          _isLoading = false;
+          update();
+          Get.snackbar("Success", 'Registration has been Successful',
+              backgroundColor: Colors.green,
+              colorText: Colors.white,
+              snackPosition: SnackPosition.BOTTOM);
+          Get.offAll(() => const LoginScreen(isComesFromRegistration: true));
+          clearTheControllers();
+          return true; //
+        });
+      } else {
+        Get.snackbar(
+          "Error Creating Account",
+          'Please enter all the fields',
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
         _isLoading = false;
         update();
-        Get.snackbar("Success", 'Success');
-        Get.offAll(()=>const LoginScreen(isComesFromRegistration:true));
-        clearTheControllers();
-        return true; //
-      });
-    }else{
-      Get.snackbar("Error Creating Account", 'Please enter all the fields');
+        return false;
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
       _isLoading = false;
       update();
       return false;
     }
-  }catch (e){
-    Get.snackbar('Error Creating Account', e.toString());
     _isLoading = false;
+
     update();
     return false;
   }
-  _isLoading = false;
-
-  update();
-  return false;
-  }
-  
-
 }
