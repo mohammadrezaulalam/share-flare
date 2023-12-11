45e5d1a9f0aa5ebc9df131e2a661e44c280f3fe6
Md. Ismail Hossain <hossain.eee042@gmail.com>
2023-12-12 02:34:13 +0600

    #jira-4-23 statemanagement controller name change according to screen file name

--- lib/presentation/ui/utilities/auth_constant.dart
+++ lib/presentation/ui/utilities/auth_constant.dart
@@ -2,8 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:share_flare/presentation/state_holders/sign_in_controller.dart';
-import 'package:share_flare/presentation/state_holders/sign_up_controller.dart';
+import 'package:share_flare/presentation/state_holders/login_controller.dart';
+import 'package:share_flare/presentation/state_holders/registration_controller.dart';
 
 //FIREBASE
 var firebaseAuth = FirebaseAuth.instance;
